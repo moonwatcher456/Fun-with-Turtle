eb4808c46e1d48dd10b4d5c97f73aeeb5fc6e64e
moonwatcher456 <kat.m.scott@outlook.com>
2016-05-06 17:37:08 -0700

    Create swampy.py

---
+++ swampy.py
@@ -0,0 +1,28 @@
+import turtle
+
+#create a turtle object
+swampy = turtle.Turtle()
+#chage speed of turtle
+swampy.speed(200)
+#change the color of turtle
+swampy.color("orange")
+swampy.pendown()
+swampy.forward(50)
+#rotate turtle direction 90 degrees
+swampy.left(90)
+swampy.forward(50)
+swampy.right(90)
+swampy.forward(50)
+swampy.right(120)
+swampy.forward(50)
+swampy.left(120)
+swampy.forward(50)
+swampy.right(180)
+swampy.forward(50)
+swampy.right(120)
+swampy.forward(50)
+swampy.right(120)
+swampy.forward(50)
+swampy.forward(50)
+swampy.left(90)
+swampy.forward(50)
